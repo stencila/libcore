152768cb88544eb01581e51ee72c450e6aa512ff
Nokome Bentley <nokome@stenci.la>
2017-10-05 16:46:32 +1300

    Implementation and tests for cor function

---
+++ r/tests/testthat/test_cor.R
@@ -0,0 +1,11 @@
+test_that("cor", {
+  expect_equal(cor(1:10, 1:10), 1)
+  expect_equal(cor(10:1, 1:10), -1)
+
+  expect_error(
+    cor(c("a", "b"), NULL),
+    "`x` parameter must be an array\\[number\\]"
+  )
+  expect_error(cor(1:10, NULL), "`y` parameter must be an array\\[number\\]")
+  expect_error(cor(1:10, 1:5), "`x` and `y` parameters must be the same length")
+})
