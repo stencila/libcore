f9c31cc5bcf45a33511d0172472fe4ff534c2617
Nokome Bentley <nokome@stenci.la>
2017-10-21 16:32:59 +0200

    Add test_close() function

---
+++ js/test/test_close.js
@@ -0,0 +1,20 @@
+import tape from 'tape'
+import test_close from '../src/test_close.js'
+
+tape('test_close', function (t) {
+  const test1 = test_close(5, 5, 0.1)
+  t.deepEqual(test1, {
+    type: 'test',
+    passed: true,
+    message: 'Passed'
+  })
+
+  const test2 = test_close(5, 0, 0.1)
+  t.deepEqual(test2, {
+    type: 'test',
+    passed: false,
+    message: 'Value 5 is not close to expected 0 with precision 0.1'
+  })
+
+  t.end()
+})
