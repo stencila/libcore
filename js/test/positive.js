abda2f37e41469a60d743999ae9679b622f39107
Nokome Bentley <nokome@stenci.la>
2017-11-22 10:43:25 +1300

    Change `positive` so only accepts `number` argument

--- js/test/positive.js
+++ js/test/positive.js
@@ -3,7 +3,10 @@ import positive from '../src/positive.js'
 
 test('positive', function (t) {
   t.equal(positive(1), 1)
-  t.deepEqual(positive([1, 2, 3]), [1, 2, 3])
+  t.equal(positive(-1), -1)
+  t.equal(positive(3.14), 3.14)
+
+  t.throws(() => positive('foo'), /parameter `value` must be a number/)
 
   t.end()
 })
