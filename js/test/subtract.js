842960ba5863e6ccd97b325021281b23527d927b
Nokome Bentley <nokome@stenci.la>
2017-11-30 17:04:18 +1300

    Add subtract function

---
+++ js/test/subtract.js
@@ -0,0 +1,13 @@
+import test from 'tape'
+import subtract from '../src/subtract.js'
+
+test('subtract', function (t) {
+  t.equal(subtract(12, 5), 7)
+  t.equal(subtract(-1, 2), -3)
+  t.equal(subtract(3.3, 1.1), 3.3 - 1.1)
+
+  t.throws(() => subtract('foo', 2), /parameter `value` must be a number/)
+  t.throws(() => subtract(2, 'foo'), /parameter `other` must be a number/)
+
+  t.end()
+})
