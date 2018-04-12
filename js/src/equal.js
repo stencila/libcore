a58fc6bfeddc776717b0791fff917fe1ddaaa01b
Aleksandra Pawlik <aleksandra.n.pawlik@gmail.com>
2018-04-12 22:28:20 +1200

    Refactor equal

--- js/src/equal.js
+++ js/src/equal.js
@@ -1,6 +1,25 @@
 import { isEqual } from 'lodash-es'
 import type from './type'
 
++/**
++@title equal
++@summary Are two values equal?.
++
++@description
++
++Performs a deep comparison between two values to determine if they are equivalent.
++
++@param {number} value The value to be compared.
++@param {number} lower The value compared to.
++@return {boolean} True if the values are equivalent, else false.
++
++@example equal(value, other)
++@example <caption>Example usage of equal function.</caption>
++@example equal(1,1)
++returns True
++ */
+
+
 export default function equal (value, other) {
   const type_ = type(value)
   if (type_ !== type(other)) return false
