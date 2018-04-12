39e0916d8ce1dfc9488ccc25b9f600c108c2e010
Aleksandra Pawlik <aleksandra.n.pawlik@gmail.com>
2018-04-12 23:07:40 +1200

    Refactor filter

--- js/src/filter.js
+++ js/src/filter.js
@@ -4,6 +4,26 @@ import is_object from './is_object'
 import is_table from './is_table'
 import table from './table'
 
++/**
++@title filter
++@summary
++
++@description
++
++Flters data from the provided value (array, table or object) matching given clause (parameters)
++and returns it as an array, table or object.
++
++@param {array|table|object} value The tested value
++@param {number} clause The lower bound of the range.
++@return {array|table|object} The array, table or object containing filtered values matching the clause.
++
++@example filter(value, clause)
++@example <caption>Example usage of filter function.</caption>
++@example filter(['a', 'b', 'c'],  'index == 2 || value == "c"')
++@example returns ['b', 'c']
++ */
+
+
 export default function filter(value, clause) {
   if (is_array(value)) return _filter_array(value, clause)
   else if (is_table(value)) return _filter_table(value, clause)
