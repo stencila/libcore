e0ec54322a3028f5c953573bd5cb77223d832847
Aleksandra Pawlik <aleksandra.n.pawlik@gmail.com>
2018-04-13 11:40:26 +1200

    Refactor less

--- js/src/less.js
+++ js/src/less.js
@@ -1,3 +1,18 @@
++/**
++@title less
++@summary Check if given value is less than the other one.
++
++
++@param {number} value The tested value
++@param {number} other The other value to test against.
++@return {boolean} True if the value is less than the other, else false.
++
++@example less(value, other)
++@example <caption>Example usage of less function.</caption>
++@example less(1,2)
++@example returns True
++ */
+
 export default function less(value, other) {
   return value < other
 }
