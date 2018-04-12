b2d16a59d9774f794f555ebc8496dd0b1539eae3
Aleksandra Pawlik <aleksandra.n.pawlik@gmail.com>
2018-04-13 11:37:22 +1200

    Refactor less_or_equal

--- js/src/less_or_equal.js
+++ js/src/less_or_equal.js
@@ -1,3 +1,18 @@
++/**
++@title less_or_equal
++@summary Check if given value is less or equal to the other one.
++
++
++@param {number} value The tested value
++@param {number} lower The other value to test against.
++@return {boolean} True if the value is lower or equal, else false.
++
++@example less_or_equal(value, other)
++@example <caption>Example usage of less_or_equal function.</caption>
++@example less_or_equal(1,2)
++@example returns True
++ */
+
 export default function less_or_equal(value, other) {
   return value <= other
 }
