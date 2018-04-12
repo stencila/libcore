90faa23a2c12648fdbfb7abe6197c3b3b6375a78
Aleksandra Pawlik <aleksandra.n.pawlik@gmail.com>
2018-04-12 14:43:38 +1200

    Refactor divide

--- js/src/divide.js
+++ js/src/divide.js
@@ -1,6 +1,28 @@
 import assert from './assert'
 import is_number from './is_number'
 
++/**
++@name divide
++@title divide
++@summary Divide two numbers
++
++@description
++
++Divide two numbers. The slash, `/`, is used as an alias for `divide` e.g.
++`x / y` is equivalent to `divide(x, y)`.
++
++@param {number} value The value to be divided
++@param {number} other The divisor
++@return {number} Division result
++
++@example divide(x, y)
++@example x / y
++
+@implem js
++
++@author Nokome Bentley
++/*
+
 export default function divide(value, other) {
   assert(is_number(value), 'parameter `value` must be a number')
   assert(is_number(other), 'parameter `other` must be a number')
