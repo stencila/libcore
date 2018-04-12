80eb9907f61653a12899b0ae98973176ee189481
Aleksandra Pawlik <aleksandra.n.pawlik@gmail.com>
2018-04-13 10:17:09 +1200

    Refactor greater_or_equal

--- js/src/greater_or_equal.js
+++ js/src/greater_or_equal.js
@@ -1,3 +1,28 @@
+
++/**
++@title greater_or_equal
++@summary Check if the given value is greater or equal, to the other value.
++
++@description
++
++Checks if the given value is greater or equal, to the other value.
++
++@name greater_or_equal
++
++@param {any} value
++@param {any} other
++@return {boolean} If the given value is greater or equal to the other value returns true, else false.
++
++@implem js
++
++@example greater_or_equal(value, other)
++@example <caption>Example usage of greater_or_equal function.</caption>
++@example greater_or_equal(2,1)
++returns True
++ */
+
+
+
 export default function greater_or_equal(value, other) {
   return value >= other
 }
