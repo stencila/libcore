c0bca4a6489ba1baaaad018735e01008fa9d036e
Aleksandra Pawlik <aleksandra.n.pawlik@gmail.com>
2018-04-13 10:50:45 +1200

    Refactor greater

--- js/src/greater.js
+++ js/src/greater.js
@@ -1,3 +1,18 @@
++/**
++@title greater
++@summary Check if given value is greater than the other.
++
++
++@param {number} value The tested value
++@param {number} other The other value to test against.
++@return {boolean} True if the value is greater than the other, else false.
++
++@example greater(value, other)
++@example <caption>Example usage of greater function.</caption>
++@example greater(2,1)
++@example returns True
++ */
+
 export default function greater(value, other) {
   return value > other
 }
