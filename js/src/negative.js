6a51db9ffad12a0a67e257916a7d9a3c8c1dc897
Aleksandra Pawlik <aleksandra.n.pawlik@gmail.com>
2018-04-13 16:29:27 +1200

    Refactor negative

--- js/src/negative.js
+++ js/src/negative.js
@@ -1,6 +1,30 @@
 import assert from './assert'
 import is_number from './is_number'
 
+
++/**
++@name negative
++@title negative
++@summary Return the negative of the value
++
++@description
++
++Returns the negative of the value. The unary negation, `-` operator is an alias for `negative` e.g. `-x` is equivalent to `negative(x)`.
++
++@related postivie
++@param {number} value The value
++@return {number} The negative of the value
++
++@example negative(x)
++@example <caption>Example usage of negative function.</caption>
++@example negative(2)
++@example returns -2
++
++@implem js
++
++@author Nokome Bentley
++ */
+
 export default function negative(value) {
   assert(is_number(value), 'parameter `value` must be a number')
   return -value
