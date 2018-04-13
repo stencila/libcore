e0e54dd16a436d9b299d05cf12ed8eb13df8bd84
Aleksandra Pawlik <aleksandra.n.pawlik@gmail.com>
2018-04-14 08:27:31 +1200

    Refactor not

--- js/src/not.js
+++ js/src/not.js
@@ -1,6 +1,30 @@
 import assert from './assert'
 import is_boolean from './is_boolean'
 
+
++/**
++@name Not
++@title Not
++@summary Logically invert an expression
++
++@description
++
++If `value` is `true` then return `false`, and if `false` then return
++`true`. The exclamation operator `!` is an alias for `not` e.g. `!(x>0)` is equivalent to `not(x>0)`.
++
++@param {boolean} value The value to be negated.
++@return {boolean} The logical inverse.
++
++@implem js
++@author Nokome Bentley
++@example not(x > 4)
++@example <caption>Example usage of not function.</caption>
+
++@example not(True)
++@example returns False
++ */
+
+
 export default function not(value) {
   assert(is_boolean(value), 'parameter `value` must be a boolean')
   return !value
