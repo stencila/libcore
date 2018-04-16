1bed28543849b4c3d25b3a533b53193b6bd25ff2
Aleksandra Pawlik <aleksandra.n.pawlik@gmail.com>
2018-04-17 08:33:06 +1200

    WIP Plotly documentation - need better desc and examples

--- js/src/plotly.js
+++ js/src/plotly.js
@@ -6,6 +6,27 @@ import is_array from './is_array'
   It is merely bringing the input data into an appropriate
   format and returns an object that is rendered by the PlotlyValueComponent.
 */
+
++/**
++@title plotly
++@summary Create a plot object that can be rendered using Plotly.
++
++@description
++
++This documentation is based on (Plotly)(https://plot.ly/javascript/).
++Creates a configuration for a Plotly plot.
++
++@param {any} traces The tested value
++@param {object} layout The lower bound of the range.
++@return {plot} True if the value is between lower and upper, else false.
++
++@example plotly(traces, layout)
++@example <caption>Example usage of plotly function.</caption>
++returns plot
++@example plotly()
++ /*
+
+
 export default function plotly (traces, layout) {
   // TODO: make it convenient to use this function using transclusions
 
