3872d93e54a887f9fe5646a8ed826aff4a96a350
Aleksandra Pawlik <aleksandra.n.pawlik@gmail.com>
2018-04-17 08:09:58 +1200

    Refactor sleep

--- js/src/sleep.js
+++ js/src/sleep.js
@@ -1,6 +1,28 @@
 import assert from './assert'
 import is_number from './is_number'
 
+
+/**
++@title sleep
++@name sleep
++@summary Delay execution for a specified time interval (in seconds).
++
++@description
++
++Delay execution for a specified time interval (in seconds).
++In Javascript this function is implemented in blocking, CPU-hogging way.
++
++@param {number}  seconds Duration of delay
++@return {number} Duration of delay
++
++@implem js
++
++@example sleep(seconds)
++@example <caption>Example usage of sleep function.</caption>
++@example sleep(20)
++ */
+
+
 export default function sleep(seconds) {
   assert(is_number(seconds), 'parameter `seconds` must be a number')
 
