30f6ddf29addca80d00af71b8e166fd8ec88e61b
Aleksandra Pawlik <aleksandra.n.pawlik@gmail.com>
2018-04-19 20:03:09 +1200

    Flatten clone

--- js/src/clone.js
+++ js/src/clone.js
@@ -1,5 +1,23 @@
 import { cloneDeep } from 'lodash-es'
 
+/**
+* @title clone
+* @name clone
+* @summary Creates a deep (recursive) clone of the value.
+*
+* @description Creates a deep (recursive) clone of the value. Uses lodash-es cloneDeep.
+*
+* @param {any} value The value to clone.
+* @return {any} Returns exact (recursive) clone of the value.
+*
+* @example clone(condition, message)
+* @example <caption>Example usage of clone function.</caption>
+*
+* @example clone([{ 'a': 1 }, { 'b': 2 }])
+* @example returns [{ 'a': 1 }, { 'b': 2 }]
+*/
+
+
 export default function clone(value) {
   return cloneDeep(value)
 }
