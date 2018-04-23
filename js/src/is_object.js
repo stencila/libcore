7de2686d7e10a522c070b179f623d97c26d0bfa2
Aleksandra Pawlik <aleksandra@stenci.la>
2018-04-24 11:43:56 +1200

    Flatten is_object and typo/formatting for aggregate

--- js/src/is_object.js
+++ js/src/is_object.js
@@ -1,5 +1,21 @@
 import { default as stdlib } from '@stdlib/stdlib'
 
+/**
+* @title is_object
+* @name is_object
+* @summary Checks if value is of type object.
+*
+*
+* @param {any} value The value to be checked.
+* @returns {boolean} True if the value is of type object, else false.
+*
+* @implem js
+* @example <caption>Example usage of is_object function.</caption>
+* // returns true
+* is_object([])
+*
+*/
+
 export default function is_object (value) {
   return stdlib.assert.isObject(value)
 }
