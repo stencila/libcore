3a332140e449ea1e15c053d78f8f0689672b238e
Aleksandra Pawlik <aleksandra.n.pawlik@gmail.com>
2018-04-24 07:35:12 +1200

    Refactor is_array

--- js/src/is_array.js
+++ js/src/is_array.js
@@ -3,6 +3,27 @@ import { default as stdlib } from '@stdlib/stdlib'
 import assert from '../src/assert'
 import is_string from '../src/is_string'
 
+
+/**
+* @title is_array
+* @name is_array
+* @summary Checks what elements the array contains.
+*
+* @description
+*
+* Checks the types of elements that the array contains. Returns true if the elements are of the given type, else returns false.
+*
+* @param {array} value The array to be checked
+* @param {string} type The type of element, by default the type is "any".
+* @return {boolean} Tue if array contains the elements of given type, else false.
+*
+* @implem js
+*
+* @example is_array([true, false], "boolean")
+* @example @returns true
+*/
+
+
 export default function is_array (value, type = "any") {
   assert(is_string(type), 'parameter `type` must be a string')
 
