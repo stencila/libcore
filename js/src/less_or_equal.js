417ebd838f1d93b6ef824d122493c19e1bf488ec
Aleksandra Pawlik <aleksandra@stenci.la>
2018-04-24 15:12:29 +1200

    Formatting and typos for less_or_equal

--- js/src/less_or_equal.js
+++ js/src/less_or_equal.js
@@ -1,17 +1,20 @@
-+/**
-+@title less_or_equal
-+@summary Check if given value is less or equal to the other one.
-+
-+
-+@param {number} value The tested value
-+@param {number} lower The other value to test against.
-+@return {boolean} True if the value is lower or equal, else false.
-+
-+@example less_or_equal(value, other)
-+@example <caption>Example usage of less_or_equal function.</caption>
-+@example less_or_equal(1,2)
-+@example returns True
-+ */
+/**
+* @title less_or_equal
+* @name less_or_equal
+* @summary Check if given value is less or equal to the other one.
+*
+*
+* @param {number} value The tested value
+* @param {number} other The other value to test against.
+* @return {boolean} True if the value is lower or equal, else false.
+*
+* @implem js
+*
+* @example less_or_equal(value, other)
+* @example <caption>Example usage of less_or_equal function.</caption>
+* // returns True
+* less_or_equal(1,2)
+*/
 
 export default function less_or_equal(value, other) {
   return value <= other
