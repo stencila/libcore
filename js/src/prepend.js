59aafa97258c1408758c671a415284c1f726d12b
Aleksandra Pawlik <aleksandra@stenci.la>
2018-04-24 15:55:05 +1200

    Format and typos prepend

--- js/src/prepend.js
+++ js/src/prepend.js
@@ -2,6 +2,30 @@ import assert from './assert'
 import clone from './clone'
 import is_array from './is_array'
 
+/**
+* @title prepend
+* @name prepend
+* @summary Prepends an element to the array.
+*
+* @description
+*
+* Prepends the element to the array and returns the new array, with the prepended item.
+*
+* @param {array} array The array.
+* @param {any} element The element to be prepended.
+* @return {array} The array with prepended element.
+*
+* @implem js
+*
+* @example prepend(array, element)
+* @example <caption>Example usage of prepend function.</caption>
+* // returns ["hello", 1, 3, 6]
+* prepend([1, 3, 6], "hello")
+*
+* @author Nokome Bentley
+*
+*/
+
 export default function prepend(value, other) {
   assert(is_array(value), 'parameter `value` must be an array')
   let result = clone(value)
