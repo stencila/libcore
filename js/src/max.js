3dc99521e1b554826a9ab250a8472c6e1c014c55
Aleksandra Pawlik <aleksandra@stenci.la>
2018-04-24 15:26:09 +1200

    Format max

--- js/src/max.js
+++ js/src/max.js
@@ -1,19 +1,21 @@
 import { default as jStat } from 'jstat'
 import { _wrap_array_number } from '../src/_helpers'
 
-+/**
-+@title max
-+@summary Maximum value of the given array of numbers. Based on jStat.max
-+
-+
-+@param {array[number]} value The tested array.
-+@return {number} The maxium value of the array.
-+
-+@example max(value)
-+@example <caption>Example usage of max function.</caption>
-+@example max([1,2])
-+@example returns 2
-+ */
+/**
+* @title max
+* @name max
+* @summary Maximum value of the given array of numbers. Based on jStat.max
+*
+*
+* @param {array[number]} value The tested array.
+* @return {number} The maxium value of the array.
+*
+* @example max(value)
+* @example <caption>Example usage of max function.</caption>
+* // returns 2
+* max([1,2])
+*
+*/
 
 export default function max(value) {
   return _wrap_array_number(jStat.max, value)
