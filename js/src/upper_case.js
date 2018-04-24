47d8fdf6ba4eb79e4820a17f83ef8055ebf311ad
Aleksandra Pawlik <aleksandra@stenci.la>
2018-04-24 21:52:28 +1200

    Refactor upper_case

--- js/src/upper_case.js
+++ js/src/upper_case.js
@@ -2,6 +2,25 @@ import assert from './assert'
 import is_string from './is_string'
 import { upperCase } from 'lodash-es'
 
+/**
+* @title upper_case
+* @name upper_case
+* @summary Changes all lower case to upper case in the string.
+*
+*
+* @param {string} value The string to have lower case changed to upper case.
+* @return {string} Returns the string with all upper case letters.
+*
+* @implem js
+*
+* @example <caption>Example usage of upper_case function.</caption>
+*
+* // returns UPPER CASE
+* lower_case("lower case")
+*
+*/
+
+
 export default function upper_case (value) {
   assert(is_string(value), 'parameter `value` must be a string')
 
