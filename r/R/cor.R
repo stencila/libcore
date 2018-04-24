bb4e57470a36c3b0ec180b7605b9dea796f2484b
Aleksandra Pawlik <aleksandra@stenci.la>
2018-04-24 13:38:06 +1200

    Refactor R Pearson correlation

--- r/R/cor.R
+++ r/R/cor.R
@@ -1,4 +1,12 @@
 cor <- function(x, y) {
+
+ #' Pearson correlation
+ #'
+ #' Calculates the Pearson correlation between x and y
+ #' @param x array[number] . The first value to be correlated
+ #' @param y array[number] . The second value to be correlated
+ #' @return number The Pearson correlation between two values.
+
   assert(is_array(x, "number"), "`x` parameter must be an array[number]")
   assert(is_array(y, "number"), "`y` parameter must be an array[number]")
   assert(length(x) == length(y), "`x` and `y` parameters must be the same length")
