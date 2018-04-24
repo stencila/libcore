33b13295cf8cbfa51d1b75f1add868f3a678a26b
Aleksandra Pawlik <aleksandra@stenci.la>
2018-04-24 14:15:20 +1200

    Formatting for between

--- js/src/between.js
+++ js/src/between.js
@@ -1,25 +1,25 @@
 import { default as stdlib } from '@stdlib/stdlib'
 
 
-+/**
-+@title between
-+@summary Check if given value is within range.
-+
-+@description
-+
-+Checks if the given value is within the defined range (between lower and upper)
-+and returns true if the value is between lower and upper, otherwise returns false.
-+
-+@param {number} value The tested value
-+@param {number} lower The lower bound of the range.
-T@param {number} upper The lower bound of the range.he upper bound of the range.
-+@return {boolean} True if the value is between lower and upper, else false.
-+
-+@example between(x, upper, lower)
-+@example <caption>Example usage of between function.</caption>
-+returns True
-+@example between(2,1,5)
-+ */
+/**
+* @title between
+* @summary Check if given value is within range.
+*
+* @description
+*
+* Checks if the given value is within the defined range (between lower and upper)
+* and returns true if the value is between lower and upper, otherwise returns false.
+*
+* @param {number} value The tested value
+* @param {number} lower The lower bound of the range.
+* @param {number} upper The lower bound of the range.he upper bound of the range.
+* @return {boolean} True if the value is between lower and upper, else false.
+*
+* @example between(x, upper, lower)
+* @example <caption>Example usage of between function.</caption>
+* // returns True
+* between(2,1,5)
+*/
 
 
 export default function between (value, upper, lower) {
