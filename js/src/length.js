59bf1ae8dd021c417b3aaf825527540ddebb3d97
Aleksandra Pawlik <aleksandra@stenci.la>
2018-04-24 15:10:39 +1200

    Refactor length

--- js/src/length.js
+++ js/src/length.js
@@ -2,6 +2,25 @@ import is_array from './is_array'
 import is_object from './is_object'
 import is_table from './is_table'
 
+
+/**
+* @title length
+* @name length
+* @summary Length of the value.
+*
+* @description Returns the length of the given value. For the array, table or object,
+* returns their actual length. For other types returns 1. 
+*
+* @param {anyt} value The value which length is to be measured.
+* @returns {number} The length of the value.
+*
+* @implem js
+*
+* @example <caption>Example usage of length function.</caption>
+* // returns 3
+* length([2, 4, "t"])
+*/
+
 export default function length(value) {
   if (is_array(value)) return value.length
   else if (is_table(value)) return value.rows
