98a5aab023141bfaa923c7d3c4c82a17fc3df3e9
Aleksandra Pawlik <aleksandra@stenci.la>
2018-04-24 14:55:48 +1200

    Formatting for is_table

--- js/src/is_table.js
+++ js/src/is_table.js
@@ -1,3 +1,20 @@
+/**
+* @title is_table
+* @name is_table
+* @summary Checks if value is of type table.
+*
+*
+* @param {any} value The value to be checked.
+* @returns {boolean} True if the value is of type table, else false.
+*
+* @implem js
+* @example <caption>Example usage of is_table function.</caption>
+* // returns true
+* is_table(table({a: [1, 2, 3]})
+*
+*/
+
+
 export default function is_table (value) {
   return typeof value === 'object' && value.type === 'table'
 }
