edc5daa0b748d15ae953bcb6625b7b7d0c3341e2
Aleksandra Pawlik <aleksandra@stenci.la>
2018-04-24 14:21:43 +1200

    Formatting for close

--- js/src/close.js
+++ js/src/close.js
@@ -1,25 +1,26 @@
 import { default as stdlib } from '@stdlib/stdlib'
 
-+/**
-+@title close
-+@summary Is a value close to another?
-+
-+@description
-+
-+Checks if the given value is close to another value, within a defined precision range
-+Returns true, tf the value is close to another within the defined precistion, else, returns false.
-+
-+
-+@param {number} value The tested value
-+@param {number} expected The value to compare to.
-T@param {number} precision The precision of the comparision.
-+@return {boolean} True if the value is within plus or minus precision of the other value, else false.
-+
-+@example close(x, upper, lower)
-+@example <caption>Example usage of close function.</caption>
-+@example close(1.2,1,0.3)
-+returns True
-+ */
+/**
+* @title close
+* @summary Is a value close to another?
+*
+* @description
+*
+* Checks if the given value is close to another value, within a defined precision range
+* Returns true, if the value is close to another within the defined precision, else, returns false.
+*
+*
+* @param {number} value The tested value
+* @param {number} expected The value to compare to.
+* @param {number} precision The precision of the comparison.
+* @return {boolean} True if the value is within plus or minus precision of the other value, else false.
+*
+* @example close(x, upper, lower)
+* @example <caption>Example usage of close function.</caption>
+* // returns True
+* close(1.2,1,0.3)
+*
+*/
 
 export default function close (value, expected, precision) {
   return stdlib.assert.isBetween(
