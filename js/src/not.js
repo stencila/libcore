70c9fa5acd166cff5d9fe75eb9608ab07f384e8a
Aleksandra Pawlik <aleksandra@stenci.la>
2018-04-24 15:42:41 +1200

    Format and typos not

--- js/src/not.js
+++ js/src/not.js
@@ -2,27 +2,27 @@ import assert from './assert'
 import is_boolean from './is_boolean'
 
 
-+/**
-+@name Not
-+@title Not
-+@summary Logically invert an expression
-+
-+@description
-+
-+If `value` is `true` then return `false`, and if `false` then return
-+`true`. The exclamation operator `!` is an alias for `not` e.g. `!(x>0)` is equivalent to `not(x>0)`.
-+
-+@param {boolean} value The value to be negated.
-+@return {boolean} The logical inverse.
-+
-+@implem js
-+@author Nokome Bentley
-+@example not(x > 4)
-+@example <caption>Example usage of not function.</caption>
-
-+@example not(True)
-+@example returns False
-+ */
+/**
+* @name Not
+* @title Not
+* @summary Logically invert an expression. (Logical negation)
+*
+* @description
+*
+* If `value` is `true` then return `false`, and if `false` then return
+* `true`. The exclamation operator `!` is an alias for `not` e.g. `!(x>0)` is equivalent to `not(x>0)`.
+*
+* @param {boolean} value The value to be negated.
+* @return {boolean} The logical inverse.
+*
+* @implem js
+* @author Nokome Bentley
+* @example not(x > 4)
+* @example <caption>Example usage of not function.</caption>
+* // returns False
+* not(True)
+*
+*/
 
 
 export default function not(value) {
