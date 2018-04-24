ca8848d52b6f03b753da321760b6ed209ee3d220
Aleksandra Pawlik <aleksandra@stenci.la>
2018-04-24 15:45:02 +1200

    Format and typos for or

--- js/src/or.js
+++ js/src/or.js
@@ -1,23 +1,27 @@
 import assert from './assert'
 import is_boolean from './is_boolean'
 
-+/**
-+@title or
-+@summary Logical OR.
-+
-+@description
-+
-+Logical OR. Returns true if either or both values are true, else returns false.
-+
-+@param {boolean} value The first boolean value.
-+@param {boolean} other The second boolean value.
-+@return {boolean} True if at least one of the values is true, else false.
-+
-+@example or(value, other)
-+@example <caption>Example usage of or function.</caption>
-+@example or(true, false)
-+@example returns True
-+ */
+/**
+* @title or
+* @name or
+* @summary Logical OR.
+*
+* @description
+*
+* Logical OR. Returns true if either or both values are true, else returns false.
+*
+* @param {boolean} value The first boolean value.
+* @param {boolean} other The second boolean value.
+* @return {boolean} True if at least one of the values is true, else false.
+*
+* @implem js
+*
+* @example or(value, other)
+* @example <caption>Example usage of or function.</caption>
+* // returns True
+* or(true, false)
+*
+*/
 
 export default function or(value, other) {
   assert(is_boolean(value), 'parameter `value` must be a boolean')
