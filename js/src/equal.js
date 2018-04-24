cbac25dec62eb2f556b90097a3d8792b9cb07754
Aleksandra Pawlik <aleksandra@stenci.la>
2018-04-24 14:28:38 +1200

    Formatting for equal

--- js/src/equal.js
+++ js/src/equal.js
@@ -1,23 +1,27 @@
 import { isEqual } from 'lodash-es'
 import type from './type'
 
-+/**
-+@title equal
-+@summary Are two values equal?.
-+
-+@description
-+
-+Performs a deep comparison between two values to determine if they are equivalent.
-+
-+@param {number} value The value to be compared.
-+@param {number} lower The value compared to.
-+@return {boolean} True if the values are equivalent, else false.
-+
-+@example equal(value, other)
-+@example <caption>Example usage of equal function.</caption>
-+@example equal(1,1)
-+returns True
-+ */
+/**
+* @title equal
+* @name equal
+* @summary Are two values equal?.
+*
+* @description
+*
+* Performs a deep comparison between two values to determine if they are equivalent.
+*
+* @param {number} value The value to be compared.
+* @param {number} lower The value compared to.
+* @return {boolean} True if the values are equivalent, else false.
+*
+* @implem js
+*
+* @example equal(value, other)
+* @example <caption>Example usage of equal function.</caption>
+* //returns True
+* equal(1,1)
+*
+*/
 
 
 export default function equal (value, other) {
