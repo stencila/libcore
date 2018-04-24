f6bf72d9f051403172e97891f92faaf831cfc952
Aleksandra Pawlik <aleksandra@stenci.la>
2018-04-24 14:26:08 +1200

    Formatting for divide

--- js/src/divide.js
+++ js/src/divide.js
@@ -1,27 +1,28 @@
 import assert from './assert'
 import is_number from './is_number'
 
-+/**
-+@name divide
-+@title divide
-+@summary Divide two numbers
-+
-+@description
-+
-+Divide two numbers. The slash, `/`, is used as an alias for `divide` e.g.
-+`x / y` is equivalent to `divide(x, y)`.
-+
-+@param {number} value The value to be divided
-+@param {number} other The divisor
-+@return {number} Division result
-+
-+@example divide(x, y)
-+@example x / y
-+
-@implem js
-+
-+@author Nokome Bentley
-+/*
+/**
+* @name divide
+* @title divide
+* @summary Divide two numbers
+*
+* @description
+*
+* Divide two numbers. The slash, `/`, is used as an alias for `divide` e.g.
+* `x / y` is equivalent to `divide(x, y)`.
+*
+* @param {number} value The value to be divided
+* @param {number} other The divisor
+* @return {number} Division result
+*
+* @example divide(x, y)
+* // return  3.3333333333333335
+* divide(10, 3)
+*
+* @implem js
+*
+* @author Nokome Bentley
+*/
 
 export default function divide(value, other) {
   assert(is_number(value), 'parameter `value` must be a number')
