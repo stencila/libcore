e0edcd47b95832b6dc7e9c0f8c11841a533e6ca0
Aleksandra Pawlik <aleksandra@stenci.la>
2018-04-24 14:40:06 +1200

    Formatting for greater

--- js/src/greater.js
+++ js/src/greater.js
@@ -1,17 +1,21 @@
-+/**
-+@title greater
-+@summary Check if given value is greater than the other.
-+
-+
-+@param {number} value The tested value
-+@param {number} other The other value to test against.
-+@return {boolean} True if the value is greater than the other, else false.
-+
-+@example greater(value, other)
-+@example <caption>Example usage of greater function.</caption>
-+@example greater(2,1)
-+@example returns True
-+ */
+/**
+* @title greater
+* @name greater
+* @summary Check if given value is greater than the other.
+*
+*
+* @param {number} value The tested value
+* @param {number} other The other value to test against.
+* @return {boolean} True if the value is greater than the other, else false.
+*
+* @implem js
+*
+* @example greater(value, other)
+* @example <caption>Example usage of greater function.</caption>
+* //  returns True
+* @example greater(2,1)
+*
+*/
 
 export default function greater(value, other) {
   return value > other
