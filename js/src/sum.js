479bb95684f762792f9a1cfa80a77ae6e5d29a14
Aleksandra Pawlik <aleksandra@stenci.la>
2018-04-24 19:08:41 +1200

    Format and small changes to sum

--- js/src/sum.js
+++ js/src/sum.js
@@ -2,22 +2,26 @@ import { default as jStat } from 'jstat'
 import { _wrap_array_number } from '../src/_helpers'
 
 /**
-+@title sum
-+@name sum
-+@summary Sum of numbers
-+
-+@description
-+
-+Sum of numbers. Uses jStat.sum. 
-+
-+@param {array.<number>} values An array of numbers to sum.
-+@return {number} The sum of the numbers.
-+
-+@example sum(value)
-+@example <caption>Example usage of sum function.</caption>
-+@example sum([1,2,3,])
-+@example returns 6
-+ */
+* @title sum
+* @name sum
+* @summary Sum of numbers
+*
+* @description
+*
+* Sum of numbers. Uses jStat.sum.
+*
+* @param {array[number]} values An array of numbers to sum.
+* @return {number} The sum of the numbers.
+*
+* @implem js
+*
+* @example sum(value)
+* @example <caption>Example usage of sum function.</caption>
+* // returns 6
+* sum([1,2,3,])
+*
+* @author Nokome Bentley
+*/
 
 export default function sum(value) {
   return _wrap_array_number(jStat.sum, value)
