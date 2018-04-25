fdd9514edab11f4c787a14602a91eb5d3d0a9197
Nokome Bentley <nokome@stenci.la>
2018-04-25 13:12:20 +1000

    Fix test

--- tests/remainder.js
+++ tests/remainder.js
@@ -7,7 +7,7 @@ test('remainder', function (t) {
   t.equal(remainder(3.3, 1.1), 3.3 % 1.1)
 
   t.throws(() => remainder('foo', 2), /parameter `value` must be a number/)
-  t.throws(() => remainder(2, 'foo'), /parameter `other` must be a number/)
+  t.throws(() => remainder(2, 'foo'), /parameter `divisor` must be a number/)
 
   t.end()
 })
